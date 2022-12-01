f9bee0853a62c35c2c20e878c5bffdc1744d2741
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2022-12-01 19:45:31 +0530

    Day-1 01-12-2022

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+class Welcome
+{
+
+public static void main(String args[])
+{
+System.out.println("welcome to carrertuner");
+
+}
+
+}
+//JDK-->JRE
+//user understable language -->machine understanable    JVM
+//binary byte 0's 1's
+
+//compiler --> compailation  javac filename.java
+//.java -->.class
+//source code-->byte code 		java Welcome
